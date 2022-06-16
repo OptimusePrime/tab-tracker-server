fa91009c74159a0aa7030f069ca1893339620695
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 13:22:11 +0200

    added basic express server; reinited eslint config to javascript modules style

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as morgan from "morgan";
+import * as cors from "cors";
+
+const app = express();
+app.use(morgan("combine"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 8081);
